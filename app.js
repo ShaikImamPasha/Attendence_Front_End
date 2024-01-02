53ed24e198d8b59f1d159a494180f296f10f9f6a
sk imam pashga <122773374+ShaikImamPasha@users.noreply.github.com>
2024-01-02 01:03:48 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+import React, { Children } from "react";
+import ReactDom from "react-dom/client";
+import { Adminpage,CreateAccount,EnableMap } from "../src/componentes/Admin/index";
+import { LoginPage } from "./componentes/LoginPage";
+import { RouterProvider,Outlet,createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utilles/ReaduxToolkit/appstore";
+import { EnterAttendence, StudentPosrtel,StudentProfile } from "./componentes/Student/index";
+
+
+const App=()=>{
+    return <>
+      <Provider store={appStore}>
+        <LoginPage />
+          <Outlet />
+      </Provider>
+    </>
+}
+const AppLayOut=createBrowserRouter([
+    {
+        path:"/",
+        element:<App />,
+        children:[
+            {
+                path: "/AdminPage",
+                element:<Adminpage />,
+                children:[
+                {
+                    path:"/AdminPage/createAccount",
+                    element:<CreateAccount />
+                },
+                {
+                    path:"/AdminPage/EnableMap",
+                    element:<EnableMap />
+                }
+               ]
+            },
+            {
+                path:"/StudentPosrtel",
+                element:<StudentPosrtel />,
+                children:[
+                    {
+                        path:"/StudentPosrtel/EnterAttendence",
+                        element:<EnterAttendence />
+                    },{
+                         path:"/StudentPosrtel/StudentProfile",
+                         element: <StudentProfile />
+                    }
+                ]
+            }
+           ]
+    }
+    
+])
+var id=document.getElementById("root");
+var root=ReactDom.createRoot(id);
+root.render(<RouterProvider router={AppLayOut}/>)
